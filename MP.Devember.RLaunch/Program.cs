bc1b5ffcdc6e34643a3d41585e6608c73b31c427
mparker42 <mparker42@hotmail.co.uk>
2021-10-31 15:27:07 +0000

    Use the component library

--- MP.Devember.RLaunch/Program.cs
+++ MP.Devember.RLaunch/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MP.Blazor.Library;
+using MP.Blazor.Library.Models;
 using MP.Devember.RLaunch;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,36 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBaseLibrary(new SiteDescription
+{
+    Pages = new[]
+    {
+        new NavigatablePage
+        {
+            Location = "/",
+            TranslationKey = "exampleHomePage"
+        },
+        new NavigatablePage
+        {
+            Location = "/counter",
+            TranslationKey = "exampleCounterPage"
+        },
+        new NavigatablePage
+        {
+            TranslationKey = "exampleFetchDataPageGrouping",
+            SubPages = new[]
+            {
+                new NavigatableSubPage
+                {
+                    Location = "/fetchdata",
+                    TranslationKey = "exampleFetchDataPage"
+                }
+            }
+        }
+    },
+    TitleTranslationKey = "exampleSiteTitle",
+    TranslationsLocation = new Uri(new Uri(builder.HostEnvironment.BaseAddress), "/translations/__locale__.json"),
+    SupportedLocales = new[] { "en-GB" }
+});
 
 await builder.Build().RunAsync();
