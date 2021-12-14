0464690895bf6fbef5d59aca2966a933fefaebb0
mparker42 <mparker42@hotmail.co.uk>
2021-12-14 00:35:59 +0000

    Initial formatted site

--- MP.Devember.RLaunch/Pages/Index.razor.cs
+++ MP.Devember.RLaunch/Pages/Index.razor.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Options;
+using MP.Blazor.Library.Services.Interfaces;
 using MP.Devember.RLaunch.Models;
 using MP.SimpleTokens.Token.Clients;
 using MP.SimpleTokens.Token.Contracts;
+using Refit;
 
 namespace MP.Devember.RLaunch.Pages
 {
@@ -14,13 +16,36 @@ namespace MP.Devember.RLaunch.Pages
         [Inject]
         public IOptions<FeaturedToken> TokenConfiguration { get; set; }
 
+        [Inject]
+        public ITranslationService? TranslationService { get; set; }
+
+        private ITranslationService TS { get => TranslationService == null ? throw new NullReferenceException() : TranslationService; }
+
         public PublicTokenSummary TokenSummary { get; set; } = new PublicTokenSummary();
+
+        public bool Loading { get; set; } = true;
 
         protected override async Task OnInitializedAsync()
         {
             var tokenId = TokenConfiguration.Value.Id;
 
-            TokenSummary = await TokenClient.GetSummary(tokenId);
+            try
+            {
+                TokenSummary = await TokenClient.GetSummary(tokenId);
+            }
+            catch (ApiException ex)
+            {
+                TokenSummary = new PublicTokenSummary
+                {
+                    Title = "Hidden level: error state",
+                    Image = "/images/rip.png",
+                    Description = "Unfortunately the NFT is currently unavailable... Oh no. Feel free to DM me on the Level One forums (User: negazippy) to investigate."
+                };
+            }
+
+            await base.OnInitializedAsync();
+
+            Loading = false;
         }
     }
 }
--- MP.Devember.RLaunch/Program.cs
+++ MP.Devember.RLaunch/Program.cs
@@ -6,6 +6,8 @@ using MP.Blazor.Library.Models;
 using MP.Devember.RLaunch;
 using MP.Devember.RLaunch.Models;
 using MP.SimpleTokens.Token.Clients;
+using MudBlazor;
+using MudBlazor.Utilities;
 using Refit;
 using System;
 using System.Net.Http;
@@ -15,37 +17,82 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var lightThemePalette = new Palette
+{
+    Primary = "#005900",
+    Secondary = "#F7C20C",
+    Tertiary = "#BB2E16",
+    Info = "#0C80DF",
+    Success = "#00C853",
+    Warning = "#FF9800",
+    Error = "#F44336",
+    TextPrimary = "#0F0F0F",
+    TextSecondary = "#424242",
+    TextDisabled = "#9F9F9F",
+    Background = "#FAFAFA"
+};
+
+lightThemePalette.AppbarBackground = lightThemePalette.Primary;
+
+
+
+var darkThemePalette = new Palette
+{
+    Black = "#27272f",
+    BackgroundGrey = "#27272f",
+    Surface = "#373740",
+    AppbarText = "rgba(255,255,255, 0.70)",
+    Primary = "#005900",
+    Secondary = "#F7C20C",
+    Tertiary = "#BB2E16",
+    Info = "#0C80DF",
+    Success = "#00C853",
+    Warning = "#FF9800",
+    Error = "#F44336",
+    TextPrimary = "#FAFAFA",
+    TextSecondary = "#E6E6E6",
+    TextDisabled = "#9F9F9F",
+    Background = "#242424",
+    DrawerBackground = "#333333",
+    ActionDisabled = "rgba(255,255,255, 0.26)",
+    ActionDisabledBackground = "rgba(255,255,255, 0.12)",
+    ActionDefault = "rgba(255, 255, 255, 0.54)",
+    Divider = "rgba(255,255,255, 0.12)",
+    DividerLight = "rgba(255,255,255, 0.06)",
+    TableLines = "rgba(255,255,255, 0.12)",
+    LinesDefault = "rgba(255,255,255, 0.12)",
+    LinesInputs = "rgba(255,255,255, 0.3)"
+};
+
+darkThemePalette.AppbarBackground = darkThemePalette.Primary;
+darkThemePalette.DrawerText = darkThemePalette.TextPrimary;
+
+
+
 builder.Services.AddBaseLibrary(
     new SiteDescription
     {
-        Pages = new[]
-        {
-            new NavigatablePage
+        Pages = null,
+        TitleTranslationKey = "siteTitle",
+        TranslationsLocation = new Uri(new Uri(builder.HostEnvironment.BaseAddress), "/translations/__locale__.json"),
+        SupportedLocales = new[] { "en-GB" },
+        Themes = new Dictionary<ThemeType, SiteTheme> {
             {
-                Location = "/",
-                TranslationKey = "exampleHomePage"
-            },
-            new NavigatablePage
-            {
-                Location = "/counter",
-                TranslationKey = "exampleCounterPage"
-            },
-            new NavigatablePage
-            {
-                TranslationKey = "exampleFetchDataPageGrouping",
-                SubPages = new[]
+                ThemeType.Dark,
+                new SiteTheme
                 {
-                    new NavigatableSubPage
-                    {
-                        Location = "/fetchdata",
-                        TranslationKey = "exampleFetchDataPage"
-                    }
+                    Palette = darkThemePalette
+                }
+            },
+            {
+                ThemeType.Light,
+                new SiteTheme
+                {
+                    Palette = lightThemePalette
                 }
             }
-        },
-        TitleTranslationKey = "exampleSiteTitle",
-        TranslationsLocation = new Uri(new Uri(builder.HostEnvironment.BaseAddress), "/translations/__locale__.json"),
-        SupportedLocales = new[] { "en-GB" }
+        }
     }
 );
 
